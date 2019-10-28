34ab0fd7f4e5f12fb96fad7998cc001088c3672c
Taylor Kraft <taylorkraft16@gmail.com>
2019-10-28 18:55:39 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -25,8 +25,8 @@ end
 
 def array_literal_matrix
   nested_array = [
-    ["Gerhard Schtitt", "Hal Incandenza", "Lyle", "Mario Incandenza", "Michael Pemulis"],
-    ["Bruce Green", "Don Gately", "Joelle van Dyne", "Kate Gompert", "Pat M."]
+    
+    ["Gerhard Schtitt", "Hal Incandenza", "Lyle", "Mario Incandenza", "Michael Pemulis"]
     ]
   # Using Array literal syntax only, build a nested array that uses the data in
   # side the ENNET_HOUSE and ENFIELD_TENNIS_ACADEMY Arrays but sorts
