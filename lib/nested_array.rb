42a4bf5dafe4fdb12be12388bfb2f0ecc6d4808a
Taylor Kraft <taylorkraft16@gmail.com>
2019-10-28 18:56:20 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -28,9 +28,6 @@ def array_literal_matrix
     ["Bruce Green", "Don Gately", "Joelle van Dyne", "Kate Gompert", "Pat M."],
     ["Gerhard Schtitt", "Hal Incandenza", "Lyle", "Mario Incandenza", "Michael Pemulis"]
     ]
-  # Using Array literal syntax only, build a nested array that uses the data in
-  # side the ENNET_HOUSE and ENFIELD_TENNIS_ACADEMY Arrays but sorts
-  # alphabetically by the first character.
 end
 
 def matrix_lookup(matrix, row, column)
